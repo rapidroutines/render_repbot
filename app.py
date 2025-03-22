f48549fcfcf315c22076b8efc8136f13de814fa8
rapidroutines <rapidroutines@gmail.com>
2025-03-22 15:06:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import math
 import time
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -1011,6 +1012,5 @@ def process_lunge(landmarks, state, current_time, rep_cooldown, hold_threshold):
 
 
 # Run the app
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
